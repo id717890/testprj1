04e81faf97b42420603845a5b1151c406440b2a5
Zamir Iusupov <jusupovz@gmail.com>
2023-10-19 19:16:47 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Grid} from '../submodule/src/components'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+        <Grid/>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
